e39eb2835ee7664366c1653b612ebd433d4e349d
Adeyemi Damilare Tomisin <142660624+dammyprolific@users.noreply.github.com>
2025-10-23 07:55:54 -0700

    my latest3 commit

--- my-react-app/src/component/user/UserInfo.jsx
+++ my-react-app/src/component/user/UserInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './UserInfo.module.css'
-import pic from '../../assets/user.png'
+import pic from "../../assets/flower.jpeg"
 
 const UserInfo = ({userInfo}) => {
   return (
